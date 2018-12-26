b994e1a37740b4404452a182ae17f00d2a6a8dc8
Vinayak Pandey <vinayakpandeyit@gmail.com>
2018-12-26 17:02:37 +0000

    adds apache recipe to recipes/default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,13 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+#
+package 'httpd'
+
+file '/var/www/html/index.html' do
+  content '<H1>Hello,World!</H1'
+end
+
+service 'httpd' do
+   action [:enable,:start]
+end
